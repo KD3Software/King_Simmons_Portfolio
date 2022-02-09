b73fa2dcd2fc8c5bef574d3e8014e7dd18a6e65e
King Simmons <kd3learn@gmail.com>
2022-02-09 09:34:54 -0600

    recommit

---
+++ king-portfolio-website/src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+function Navbar(){
+    const[click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        KING SIMMONS <i class="fa-solid fa-crown"></i>
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className = {click ? 'nav-menu active' : 'nav-menu'} />
+                    <li className = 'nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className = 'nav-item'>
+                        <Link to='/resume' className='nav-links' onClick={closeMobileMenu}>
+                            Resume
+                        </Link>
+                    </li>
+                    <li className = 'nav-item'>
+                        <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
+                            Contact
+                        </Link>
+                    </li>
+                </div>
+            </nav>
+
+        </>
+    )
+}
+
+export default Navbar
